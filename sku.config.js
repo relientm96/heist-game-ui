a1f9bc47f5b57aa4d950c9a319cc2e44ec21b0b6
Matthew Yong <myong@seek.com.au>
2021-05-28 20:53:57 +1000

    Adding public path to point assets

--- sku.config.js
+++ sku.config.js
@@ -2,6 +2,6 @@ module.exports = {
   clientEntry: 'src/client.tsx',
   renderEntry: 'src/render.tsx',
   sites: [{ name: 'apac', host: 'heist-game-ui.com' }],
-  publicPath: '/', // <-- Required for sku build output
-  orderImports: true,
+  publicPath: '../', // <-- Required for sku build output
+  routes: ['/'],
 };
